013b8f30ff1f6b87bd412942dc9d8fb549a77e2c
likun <likun2@iie.ac.cn>
2020-08-12 09:20:30 +0800

    init

--- lecture3.py
+++ lecture3.py
@@ -20,9 +20,28 @@ def svm_loss_single_matrix(x, y, W):
     return loss
 
 def batch_loss_single_matrix(X, y, W):
+    # TODO
     delta = 1
     scores = W.dot(X.transpose())
+
     y_score = scores[y, :]
+
     losses = np.maximum(0, scores - y_score + delta)
     loss = np.sum(losses, axis=1)
     return loss
+
+def softmax(x, y, W):
+    scores = W.dot(x)
+    exp_score = np.exp(scores)
+    losses = exp_score / np.sum(exp_score)
+    log_losses = -np.log(losses)
+    loss = log_losses[y]
+    print(loss)
+    return loss
+
+if __name__ == '__main__':
+    x = np.array([1, -15, 22, -44, 56])
+    W = np.array([[0, 0.2, -0.3], [0.01, 0.7, 0], [-0.05, 0.2, -0.45],[0.1, 0.05, -0.2],[0.05, 0.16, 0.03]])
+    W = W.transpose()
+    y = 2
+    softmax(x, y, W)
